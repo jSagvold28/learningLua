974bcdfbdcdfa9714a367b8f40d5be7ee53e5efd
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-08 14:57:01 -0600

    moved: functions

---
+++ course1/function.lua
@@ -0,0 +1,14 @@
+local function functionName()
+
+    -- Varibles
+
+    a = 0
+    b = 5
+    c = 10
+    d = 15
+    e = 20
+
+print(a + b + c + d + e)
+
+end
+functionName() -- make sure you do this to call the function
