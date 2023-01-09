da8e312c7532d0666ca90f7ddc87843c60aa411e
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-08 20:22:22 -0600

    added without function

--- course1/loops.lua
+++ course1/loops.lua
@@ -4,13 +4,23 @@ local function loopFunction()
 
     -- Varibles for the loop
 
-    local randomGeneratedNumber = math.random(1, 15)
-    print(randomGeneratedNumber)
+    local result1 = math.random(1, 15)
+    print(result1)
     
     os.execute("sleep 5")
 
-    local highRandomNumberGenerator = math.random(16, 35)
-    print(highRandomNumberGenerator)
-    
+    local result2 = math.random(16, 35)
+    print(result2)
+
 end
 loopFunction()
+
+-- without funcion
+
+local result3 = math.random(1, 15)
+print(result3)
+
+os.execute("sleep 5")
+
+local result4 = math.random(16, 35)
+print(result4)
