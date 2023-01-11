691f6282e2706754875a4897782b607723534161
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-11 08:08:36 -0600

    may have bug

---
+++ practicingTheBasics/randomNumberGenerator.lua
@@ -0,0 +1,14 @@
+
+local function numberGenerator()
+
+    local lowRandomNumber = math.random(0, 25)
+
+    local mediumRandomNumber = math.random(26, 100)
+
+    local highNumberGenerator = math.random(101, 500)
+
+    local numberResults = lowRandomNumber, mediumRandomNumber, highNumberGenerator
+
+    print(results)
+
+end
