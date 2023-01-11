8f0533428a1554755b122ea33989e8d533b2de07
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-10 20:50:13 -0600

    no commit message

---
+++ luaCodeFiles/elseif.lua
@@ -0,0 +1,11 @@
+x = 5
+y = 10
+local generatedError = error("Something is not working. Try again later.")
+
+if x < y then
+    print("x is less than y")
+elseif x > y then
+    print("x is greater than y")
+else
+    print("x is equal to y")
+end
