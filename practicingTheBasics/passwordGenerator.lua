df219cd3e92fb4720dab291e4c3c99c9e667975f
Jayce Sagvold <97198910+jSagvold28@users.noreply.github.com>
2023-01-11 10:07:32 -0600

    bug fix

---
+++ practicingTheBasics/passwordGenerator.lua
@@ -0,0 +1,16 @@
+math.randomseed(os.time())
+
+local function generatePassword(length)
+    -- default length is 8 if no parameter passed
+    length = length or 8
+    local password = ""
+    local characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()[]}{?><"
+
+    for i = 1, length do 
+        local char = characters:sub(math.random(1, #characters), math.random(1, #characters))
+        password = password .. char
+    end
+    return password
+end
+
+print("Your generated password is: " .. generatePassword(1))
